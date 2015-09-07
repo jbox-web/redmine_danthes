ee00a47e8a090c48ca382977301c71fe513be838
Nicolas Rodriguez <nrodriguez@jbox-web.com>
2015-09-07 16:37:48 +0200

    This is stable branch

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :redmine_danthes do
   name 'Redmine Danthes'
   author 'Nicolas Rodriguez'
   description 'This is a Danthes plugin for Redmine'
-  version '1.0.0-devel'
+  version '1.0.0'
   url 'https://github.com/jbox-web/redmine_danthes'
   author_url 'https://github.com/jbox-web'
 end
