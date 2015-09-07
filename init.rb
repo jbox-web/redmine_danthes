9151373f942f3c9f76a01c44d0328a0a34e80a97
Nicolas Rodriguez <nrodriguez@jbox-web.com>
2015-09-07 16:25:25 +0200

    Bump to version 1.0.0

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :redmine_danthes do
   name 'Redmine Danthes'
   author 'Nicolas Rodriguez'
   description 'This is a Danthes plugin for Redmine'
-  version '0.1.0'
+  version '1.0.0-devel'
   url 'https://github.com/jbox-web/redmine_danthes'
   author_url 'https://github.com/jbox-web'
 end
